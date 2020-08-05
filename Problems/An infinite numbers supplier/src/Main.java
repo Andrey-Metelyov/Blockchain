bc602571d94a5951a176752d1de59552ba9acc5b
Andrey.Metelyov <andrey.metelyov@gmail.com>
2020-08-05 17:18:29 +0900

    3/6 + problems

---
+++ Problems/An infinite numbers supplier/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.function.*;
+
+class FunctionUtils {
+
+    public static Supplier<Integer> getInfiniteRange() {
+        // write your code here
+        return new Supplier<Integer>() {
+            int number = 0;
+            @Override
+            public Integer get() {
+                return number++;
+            }
+        };
+    }
+
+}
